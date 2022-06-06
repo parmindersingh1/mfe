ec3f6ab6a70f5fee6ea1f434e2bfabbd5a01f37e
Parminder Singh <parminder.s@gmail.com>
2022-06-06 17:55:39 +0530

    class name conflict fix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
-  return <>
-    <h1>Hi There</h1>
-    <hr />
-    <MarketingApp />;
-  </>
-  
+  return (
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
+  );
 };
