26e522818b99e97b143d7f360ed9c6f34d6a990d
Parminder Singh <parminder.s@gmail.com>
2022-06-07 11:59:59 +0530

    auth app isolation

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -2,10 +2,13 @@ const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
-const  packageJson = require("./../package.json");
+const packageJson = require("./../package.json");
 
 const devConfig = {
   mode: "development",
+  output: {
+    publicPath: "http://localhost:8081/",
+  },
   devServer: {
     port: 8081,
     historyApiFallback: {
@@ -21,7 +24,7 @@ const devConfig = {
         "./MarketingApp": "./src/bootstrap",
       },
       // shared: ['react', 'react-dom']
-      shared: packageJson.dependencies
+      shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
