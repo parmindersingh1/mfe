b6a2e9b490ddc5b56c804e07a0d82b15379f8fb4
Parminder Singh <parminder.s@gmail.com>
2022-06-07 13:24:37 +0530

    added auth state and workflow

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,25 +1,32 @@
 import React from "react";
 import { Switch, Route, BrowserRouter, Router } from "react-router-dom";
-import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'au'
+  productionPrefix: "au",
 });
 
-export default ({ history }) => {
+export default ({ history, onSignIn }) => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
         {/* <BrowserRouter> */}
         <Router history={history}>
           <Switch>
-            <Route path="/auth/signin" component={Signin} />
-            <Route path="/auth/signup" component={Signup} />
+            <Route path="/auth/signin">
+              <Signin onSignIn={onSignIn} />
+            </Route>
+            <Route path="/auth/signup">
+              <Signup onSignIn={onSignIn} />
+            </Route>
           </Switch>
-        {/* </BrowserRouter> */}
+          {/* </BrowserRouter> */}
         </Router>
       </StylesProvider>
     </div>
